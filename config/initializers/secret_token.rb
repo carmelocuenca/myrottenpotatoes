bcb2e74dbb9e754005a742020ebb4dce256bb9f3
OpenShift guest <carmelocuenca@hotmail.com>
2014-07-14 14:33:46 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myrottentpotatoes::Application.config.secret_key_base = '3b42c2891d0daf53ae12ab4fb37238f9cc71001cb11c9f1929cd0106b7b82088cd42d819b17314b3708166c142a37768b18f0e9d98ac243ad0924df2b71720af'
